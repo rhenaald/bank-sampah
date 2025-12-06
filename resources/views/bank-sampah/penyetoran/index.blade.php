c724ec1daa823bc5218cfea1e80e5c6ce5635578
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-12-06 21:36:17 +0700

    Update index.blade.php

--- resources/views/bank-sampah/penyetoran/index.blade.php
+++ resources/views/bank-sampah/penyetoran/index.blade.php
@@ -157,10 +157,6 @@
                                             </div>
                                         </td>
                                         <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <a href="{{ route('bank-sampah.penyetoran.show', $item) }}" 
-                                               class="mr-3 text-blue-600 hover:text-blue-900">
-                                                Detail
-                                            </a>
                                             <a href="{{ route('bank-sampah.penyetoran.edit', $item) }}" 
                                                class="mr-3 text-indigo-600 hover:text-indigo-900">
                                                 Edit
